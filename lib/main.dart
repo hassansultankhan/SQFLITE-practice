f20aa867858efab5b6d1116b3d00967ad0c089a4
Hassan Sultan Khan <hassansultankhan@gmail.com>
2022-07-29 17:17:13 +0500

    start

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    title: "Sqflite practice",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        colorScheme: const ColorScheme(
+          primary: Color.fromARGB(255, 246, 244, 229),
+          secondary: Color.fromARGB(255, 112, 220, 100),
+          surface: Colors.grey,
+          background: Color.fromARGB(255, 255, 255, 255),
+          error: Colors.grey,
+          onPrimary: Colors.deepOrange,
+          onSecondary: Colors.grey,
+          onSurface: Colors.grey,
+          onBackground: Colors.grey,
+          onError: Colors.grey,
+          brightness: Brightness.light,
+  ),
+  ),
+  ),
+  );
+}
+
